f4de122e76a13fda87ace59650e04e9270a8682a
Bart Mesuere <Bart.Mesuere@UGent.be>
2015-06-08 16:06:09 +0200

    add optional file name to the config object

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -3,8 +3,15 @@ module Unipept
     attr_reader :config
     attr_reader :file_name
 
-    def initialize
-      @file_name = File.join(Dir.home, '.unipeptrc')
+    # Creates a new config object, based on a given YAML file. If no filename
+    # given, '.unipeptrc' in the home dir of the user will be used.
+    #
+    # If the file doesn't exist, an empty config will be loaded.
+    #
+    # @param [String] file An optional file name of the YAML file to create the
+    # config from
+    def initialize(file = nil)
+      @file_name = file ? file : File.join(Dir.home, '.unipeptrc')
       if !File.exist? file_name
         @config = {}
       else
@@ -12,14 +19,18 @@ module Unipept
       end
     end
 
+    # Saves the config to disk. If the file doesn't exist yet, a new one will be
+    # created
     def save
       File.open(file_name, 'w') { |f| f.write config.to_yaml }
     end
 
+    # forwards [] to the internal config hash
     def [](*args)
       config.[](*args)
     end
 
+    # forwards =[] to the internal config hash
     def []=(*args)
       config.[]=(*args)
     end
