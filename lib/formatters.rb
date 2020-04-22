49a0e49eaf3e329e3f88a690f4c33df7dc33a2ce
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-04-22 13:18:21 +0200

    fix url output

--- lib/formatters.rb
+++ lib/formatters.rb
@@ -405,8 +405,7 @@ module Unipept
     #
     # @return [String] The converted input data in the Blast format
     def convert(data, _first)
-      data = JSON.parse(data)
-      data['gist'].sub!('https://gist.github.com/', 'https://bl.ocks.org/') + "\n"
+      data[0]['gist'].sub!('https://gist.github.com/', 'https://bl.ocks.org/') + "\n"
     end
   end
 end
