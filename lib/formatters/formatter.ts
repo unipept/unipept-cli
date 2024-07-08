beb414c44b5f5d99528522d1b7660dd55c5d4b84
Bart Mesuere <Bart.Mesuere@UGent.be>
2024-07-08 11:00:57 +0200

    fix lint

--- lib/formatters/formatter.ts
+++ lib/formatters/formatter.ts
@@ -1,19 +1,18 @@
-import { CSVFormatter } from "./csv_formatter.js";
-
 export abstract class Formatter {
 
   abstract header(sampleData: object, fastaMapper?: boolean): string;
   abstract footer(): string;
-  abstract convert(data: any, first?: boolean): string;
+  abstract convert(data: { [key: string]: string }[], first?: boolean): string;
 
-  format(data, fastaMapper?: boolean, first?: boolean): string {
+  format(data: { [key: string]: string }[], fastaMapper?: boolean, first?: boolean): string {
     if (fastaMapper) {
       data = this.integrateFastaHeaders(data, fastaMapper);
     }
     return this.convert(data, first);
   }
 
-  integrateFastaHeaders(data: any, fastaMapper: boolean): any {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  integrateFastaHeaders(data: { [key: string]: string }[], fastaMapper: boolean): { [key: string]: string }[] {
     return data;
   }
 }
